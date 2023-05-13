ad5f1e83a0356ef45b45b913e05180bdc270a6c7
Tommy Tran <haitran1995@gmail.com>
2023-05-13 08:02:32 -0400

    Add: Navbar logo and icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { MdFingerprint } from "react-icons/md";
+import { Fabars, FaTimes } from "react-icons/fa";
 
 const Navbar = () => {
   return (
@@ -6,8 +9,10 @@ const Navbar = () => {
       <div className="navbar">
         <div className="navbar-container container">
           <Link to="/" className="navbar-logo">
+            <MdFingerprint className="navbar-icon" />
             LAVISH
           </Link>
+          <div className="menu-icon"></div>
         </div>
       </div>
     </>
