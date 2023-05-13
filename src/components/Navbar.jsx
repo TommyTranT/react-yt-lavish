3ebc5705387853d5067c0275dc24cc052243d6ea
Tommy Tran <haitran1995@gmail.com>
2023-05-13 08:35:56 -0400

    Add: Fabar & FaTimes icons with js function

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { MdFingerprint } from "react-icons/md";
-import { Fabars, FaTimes } from "react-icons/fa";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const Navbar = () => {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+
   return (
     <>
       <div className="navbar">
@@ -12,7 +16,9 @@ const Navbar = () => {
             <MdFingerprint className="navbar-icon" />
             LAVISH
           </Link>
-          <div className="menu-icon"></div>
+          <div className="menu-icon" onClick={handleClick}>
+            {click ? <FaTimes /> : <FaBars />}
+          </div>
         </div>
       </div>
     </>
