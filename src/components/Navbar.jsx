e4b7fdb75f6fe32b7c3d3b2bc05957714f47e2cb
Tommy Tran <haitran1995@gmail.com>
2023-05-13 07:55:29 -0400

    Replaced: Navbar js for Navbar jsx

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="navbar">
+        <div className="navbar-container container">
+          <Link to="/" className="navbar-logo">
+            LAVISH
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
